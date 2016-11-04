161871d949ca92174431d32d1dbb42862628acc2
Mayra Salazar <mayrasalazar@unitec.edu>
2016-11-04 15:27:56 -0600

    Movimiento antihorario done

--- main.cpp
+++ main.cpp
@@ -51,7 +51,8 @@ int main(int argc, char const *argv[]) {
 
 
     imprimir(frontal,trasera,superior,inferior,izquierda,derecha);
-    moverHorario(frontal,derecha,izquierda,superior,inferior);
+    antiHorario(frontal,derecha,izquierda,superior,inferior);
+    cout<<endl;
     imprimir(frontal,trasera,superior,inferior,izquierda,derecha);
     deleteAll(frontal,trasera,superior,inferior,izquierda,derecha);
     return 0;
@@ -59,7 +60,7 @@ int main(int argc, char const *argv[]) {
 
 
 void imprimir(char** frontal,char** trasera,char** superior,char** inferior,char** derecha,char** izquierda){
-
+    cout<<"Cara Frontal"<<endl;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cout <<"["<< frontal[i][j] <<"]";
@@ -67,6 +68,7 @@ void imprimir(char** frontal,char** trasera,char** superior,char** inferior,char
         cout<<endl;
     }
     cout<<endl;
+    cout<<"Cara Trasera"<<endl;
      for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cout <<"["<< trasera[i][j] <<"]";
@@ -74,6 +76,7 @@ void imprimir(char** frontal,char** trasera,char** superior,char** inferior,char
         cout<<endl;
     }
     cout<<endl;
+    cout<<"Cara Superior"<<endl;
      for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cout <<"["<< superior[i][j] <<"]";
@@ -81,6 +84,7 @@ void imprimir(char** frontal,char** trasera,char** superior,char** inferior,char
         cout<<endl;
     }
     cout<<endl;
+    cout<<"Cara Inferior"<<endl;
      for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cout <<"["<< inferior[i][j] <<"]";
@@ -88,6 +92,7 @@ void imprimir(char** frontal,char** trasera,char** superior,char** inferior,char
         cout<<endl;
     }
     cout<<endl;
+    cout<<"Cara Derecha"<<endl;
      for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cout <<"["<< derecha[i][j] <<"]";
@@ -95,6 +100,7 @@ void imprimir(char** frontal,char** trasera,char** superior,char** inferior,char
         cout<<endl;
     }
     cout<<endl;
+    cout<<"Cara Izquierda"<<endl;
      for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cout <<"["<< izquierda[i][j] <<"]";
@@ -154,16 +160,40 @@ void moverHorario(char** frontal , char** derecha, char** izquierda,  char** sup
     }
 
     int n = 3;
-           for(int i = 0; i < n; ++i) {
-               for(int j = 0; j < n / 2; ++j) {
-                   char temp = frontal[i][n - j - 1];
-                   frontal[i][n - j - 1] = frontal[i][j];
-                   frontal[i][j] = temp;
-               }
-           }
-       }
-
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n / 2; ++j) {
+            char temp = frontal[i][n - j - 1];
+            frontal[i][n - j - 1] = frontal[i][j];
+            frontal[i][j] = temp;
+        }
+    }
+    delete[] datosSuperior;
+    delete[] datosInferior;
+    delete[] datosDerecha;
+    delete[] datosIzquierda;
 }
-void antiHorario(char** frontal,char** derecha,char** izquierda,char** superior, char** inferior){
 
+void antiHorario(char** frontal,char** derecha,char** izquierda,char** superior, char** inferior){
+    int * datosSuperior = new int[3];
+    int * datosInferior = new int[3];
+    int * datosDerecha = new int[3];
+    int * datosIzquierda = new int[3];
+    for (int i = 0; i < 3; ++i)
+    {
+        datosSuperior[i] = superior[2][i];
+        datosInferior[i] = inferior[2][i];
+        datosDerecha[i] =  derecha[i][0];
+        datosIzquierda[i] = izquierda[i][0];
+    }
+    for (int i = 0; i < 3; ++i)
+    {
+        derecha[i][0] = datosInferior[i];
+        superior[2][i]= datosDerecha[i];
+        izquierda[i][2] = datosSuperior[i];
+        inferior[0][i] = datosIzquierda[i];
+    }
+    delete[] datosSuperior;
+    delete[] datosInferior;
+    delete[] datosDerecha;
+    delete[] datosIzquierda;
 }
